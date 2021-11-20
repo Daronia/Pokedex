50363cbbaf70842dacbdd6c68ea66c75150e8d71
Daronia <Laura.haenel1997@web.de>
2021-11-20 01:17:25 +0100

    Single Pokemon data loading with prev/next button

--- reactPokedex/pokedex/src/App.js
+++ reactPokedex/pokedex/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]); // handling pokemon json
+    const [count, setCount] = useState(1); // id of pokemon which is shown
+
+    const pokeBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+    // fetch poke data
+    useEffect(() => {
+        fetch(pokeBaseUrl + count.toString())
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setItems(result);
+                    setIsLoaded(true);
+                },
+                (error) => {
+                    setError(error);
+                    setIsLoaded(true);
+                }            )
+    }, [count]) // only re-run effect if count changes
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <header className="App-header">
+              <h1>Pokedex</h1>
+
+              <div className="Catalog">
+                  <button onClick={() => {
+                      if (count > 1) setCount(count - 1);
+                  }
+                  }>Previous</button>
+                  <button onClick={() => {
+                      if (count < 150) setCount(count + 1);
+                  }
+                  }>Next</button>
+                  <table>
+                      <thead>
+                          <tr>
+                              <th>Picture</th>
+                              <th>Name</th>
+                              <th>Number</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td id="pic"><img src={items.sprites.front_default} alt={items.name} /></td>
+                              <td id="name">{items.name}</td>
+                              <td id="number">{items.id}</td>
+                          </tr>
+                      </tbody>  
+                  </table>
+              </div>
+
+          </header>
     </div>
-  );
+    );
 }
+
 
 export default App;
